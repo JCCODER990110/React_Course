36c9aa75e2171ed62d2c5eeefb5ef85855440ca5
JCCODER990110 <jccoder990110@outlook.com>
2024-09-05 17:38:22 -0500

    Second Commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+import ReactDOM from 'react-dom/client' 
+
+/**
+ 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<button>Hola Button</button>)
